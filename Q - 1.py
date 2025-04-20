aef5b75bbfa77ff0345674a92da95a57fc9b24f3
vedpatel-10 <24bee209@spt.pdpu.ac.in>
2025-04-20 12:48:14 +0530

    Update Q - 1.py

--- Q - 1.py
+++ Q - 1.py
@@ -6,3 +6,60 @@ print(" ")
 print("all alphabets in upper case:")
 for j in range(97,123):
     print(chr(j))    
+
+#OUTPUT:
+# ALL ALPHABETS IN LOWER CASE:
+# A
+# B
+# C
+# D
+# E
+# F
+# G
+# H
+# I
+# J
+# K
+# L
+# M
+# N
+# O
+# P
+# Q
+# R
+# S
+# T
+# U
+# V
+# W
+# X
+# Y
+# Z
+
+# all alphabets in upper case:
+# a
+# b
+# c
+# d
+# e
+# f
+# g
+# h
+# i
+# j
+# k
+# l
+# m
+# n
+# o
+# p
+# q
+# r
+# s
+# t
+# u
+# v
+# w
+# x
+# y
+# z  
